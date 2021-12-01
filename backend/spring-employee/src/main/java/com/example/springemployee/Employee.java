b9d892373750074dc880904b7b97275f4d52ec87
Richard Tumaneng <ren.tumaneng@gmail.com>
2021-12-01 03:06:11 -0800

    Getter Setters for Employee class

--- backend/spring-employee/src/main/java/com/example/springemployee/Employee.java
+++ backend/spring-employee/src/main/java/com/example/springemployee/Employee.java
@@ -22,4 +22,44 @@ public class Employee {
     @Column(nullable=false) private String lastName ;
     @Column(nullable=false) private String password ;
 
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmployeeID() {
+        return this.employeeID;
+    }
+
+    public void setEmployeeID(String employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
