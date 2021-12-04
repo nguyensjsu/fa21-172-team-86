181be4ae6676b23dd7ae42c08dae406f033edd8b
Cuong Ly <cuonglyy@gmail.com>
2021-12-03 23:51:49 -0800

    Added backoffice

---
+++ spring-backoffice/src/main/java/com/example/springbackoffice/SpringBackofficeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbackoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBackofficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBackofficeApplication.class, args);
+	}
+
+}
