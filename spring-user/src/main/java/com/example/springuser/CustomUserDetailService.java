618b931bc39c5216ddacd04dd1303257c1598bcc
Richard Tumaneng <ren.tumaneng@gmail.com>
2021-12-04 03:33:15 -0800

    Merge Main into my branch

--- spring-user/src/main/java/com/example/springuser/CustomUserDetailService.java
+++ spring-user/src/main/java/com/example/springuser/CustomUserDetailService.java
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-
-=======
 /*
->>>>>>> main
 package com.example.springuser;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,19 +7,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-<<<<<<< HEAD
-/*
-    https://www.baeldung.com/spring-security-authentication-with-a-database
-    Tells spring security to look up user information
-    In this case, I want to look up the user by email (or username)
-*/
-=======
 
     https://www.baeldung.com/spring-security-authentication-with-a-database
     Tells spring security to look up user information
     In this case, I want to look up the user by email (or username)
 
->>>>>>> main
 
 @Service
 public class CustomUserDetailService implements UserDetailsService {
@@ -36,24 +24,8 @@ public class CustomUserDetailService implements UserDetailsService {
         User user = userRepo.findByEmail(email) ;
 
         //Check to see if they exist
-<<<<<<< HEAD
-        if(user != null){
-            return CustomUserDetails(user);
-        }
-        throw new UsernameNotFoundException("Could not find user with the email: " + email + ".");
-=======
-        if (user == null) {
-            System.out.println("Email not found!") ;
-            throw new UsernameNotFoundException("Email not found!") ;
-        }
-
-        return new CustomUserDetails(user) ;
->>>>>>> main
     }
 
     
 }
-<<<<<<< HEAD
-=======
 */
->>>>>>> main
--- spring-user/src/main/java/com/example/springuser/CustomUserDetails.java
+++ spring-user/src/main/java/com/example/springuser/CustomUserDetails.java
@@ -1,24 +1,14 @@
-<<<<<<< HEAD
- package com.example.springuser;
-=======
 /*
 package com.example.springuser;
->>>>>>> main
 
 import java.util.Collection;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-<<<<<<< HEAD
-/*
-    https://docs.spring.io/spring-security/site/docs/current/api/org/springframework/security/core/userdetails/UserDetails.html
-*/
-=======
 
     https://docs.spring.io/spring-security/site/docs/current/api/org/springframework/security/core/userdetails/UserDetails.html
 
->>>>>>> main
 
 public class CustomUserDetails implements UserDetails {
 
@@ -74,9 +64,5 @@ public class CustomUserDetails implements UserDetails {
         return true;
     }
     
-<<<<<<< HEAD
-}
-=======
 }
 */
->>>>>>> main
