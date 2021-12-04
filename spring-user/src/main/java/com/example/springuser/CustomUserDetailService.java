74ce1d5aed7b134bb54af4a113c55f013d067c91
Richard Tumaneng <ren.tumaneng@gmail.com>
2021-12-04 02:05:26 -0800

    CustomeUserDetails and Service uncommented

---
+++ spring-user/src/main/java/com/example/springuser/CustomUserDetailService.java
@@ -0,0 +1,34 @@
+
+package com.example.springuser;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/*
+    https://www.baeldung.com/spring-security-authentication-with-a-database
+    Tells spring security to look up user information
+    In this case, I want to look up the user by email (or username)
+*/
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+    
+    @Autowired
+    private UserRepository userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepo.findByEmail(email) ;
+
+        //Check to see if they exist
+        if(user != null){
+            return CustomUserDetails(user);
+        }
+        throw new UsernameNotFoundException("Could not find user with the email: " + email + ".");
+    }
+
+    
+}
